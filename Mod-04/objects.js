15ab440e05047fe64999026dcf77eadc59151cf8
Marcos Vinicius <marcos.mva18@hotmail.com>
2021-08-25 21:34:34 -0300

    atualizando objects

--- Mod-04/objects.js
+++ Mod-04/objects.js
@@ -1,4 +1,23 @@
 /* 
-ao inves de utilizar muitas variáveis
+Ao inves de utilizar muitas variáveis com vários tipos de dados, deve-se criar uma única estrutura de dados
+com todas as propriedades de um cliente 
+
+Criar um objeto com chaves e valores
+
+JSON
+
+Obter um valor com o . (ponto) ou nome da chave em [''] colchetes
+
 
 */
+
+const pessoa = {
+    id: "0011AABBCC",
+    nome: "Negueba Junior",
+    idade: 25,
+    dataNascimento: new Date(2000, 6, 1),
+}
+
+pessoa.id
+pessoa['nome']
+pessoa['nomeDaMae'] = 'Maria Jose'
