c70c23b85c9b69df825bdadca4c748b06af3aef3
Marcos Vinicius <marcos.mva18@hotmail.com>
2021-08-26 21:13:13 -0300

    aaaaa

---
+++ mod-05/exemplo-metodos.js
@@ -0,0 +1,13 @@
+const pessoa = 
+{
+    _nome: "", 
+    get nome () {
+        return this._nome
+    },
+    set nome(valor) {
+        this._nome = valor.toUpperCase() 
+    },
+}
+
+pessoa.nome = "marcos vinicius"
+console.log(pessoa.nome)
